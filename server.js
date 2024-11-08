3b93afbf4f47ec4a26b87292283cb696952f50f7
Arun <133745182+codewitharunofficial@users.noreply.github.com>
2024-11-08 13:55:30 +0530

    Add Post fetching

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import axios from "axios";
+import cors from "cors";
 
 const app = express();
 const PORT = 8081;
+
+app.use(cors({ origin: "*" }));
 
 app.post("/user/:username", async (req, res) => {
   try {
@@ -21,7 +24,7 @@ app.post("/user/:username", async (req, res) => {
 
     try {
       const { data } = await axios.request(options);
-      
+
       if (data) {
         const imageBuffer = await axios.get(data[0]?.profile_pic_url, {
           responseType: "arraybuffer",
@@ -40,14 +43,23 @@ app.post("/user/:username", async (req, res) => {
             following: data[0]?.following_count,
             bio: data[0]?.biography,
             id: data[0]?.pk,
+            posts_count: data[0]?.media_count
           },
         });
       }
     } catch (error) {
-      console.error(error);
+      res.status(500).send({
+        success: false,
+        message: "Something went wrong",
+        error,
+      });
     }
   } catch (error) {
-    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
   }
 });
 
@@ -76,10 +88,18 @@ app.post("/stories/:username", async (req, res) => {
         stories: data,
       });
     } catch (error) {
-      console.error(error);
+      res.status(500).send({
+        success: false,
+        message: "Something went wrong",
+        error,
+      });
     }
   } catch (error) {
-    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
   }
 });
 
@@ -100,7 +120,7 @@ app.post("/highlights/:id", async (req, res) => {
         },
         headers: {
           "x-rapidapi-key":
-            "63082bf975mshaf2e4ae44199d66p180054jsne3e705022ef1",
+            "b1c26628e0msh3fbbf13ea24b4abp184561jsna2ebae86e910",
           "x-rapidapi-host":
             "instagram-scrapper-posts-reels-stories-downloader.p.rapidapi.com",
         },
@@ -114,11 +134,14 @@ app.post("/highlights/:id", async (req, res) => {
           highlights: data?.tray,
         });
       } catch (error) {
-        console.error(error);
+        res.status(500).send({
+          success: false,
+          message: "Something went wrong",
+          error,
+        });
       }
     }
   } catch (error) {
-    console.log(error);
     res.send(500).send({
       success: false,
       message: "Something went wrong",
@@ -139,13 +162,92 @@ app.post("/highlight-cover/:url", async (req, res) => {
     } else {
       const imageBuffer = await axios.get(url, { responseType: "arraybuffer" });
 
-      const image_base64 = Buffer.from(imageBuffer.data, "binary").toString("base64");
+      const image_base64 = Buffer.from(imageBuffer.data, "binary").toString(
+        "base64"
+      );
 
-      res.send(`data:${imageBuffer.headers["content-type"]};base64,${image_base64}`);
-      
+      res.send(
+        `data:${imageBuffer.headers["content-type"]};base64,${image_base64}`
+      );
     }
   } catch (error) {
     console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
+});
+
+app.use("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const options = {
+      method: "GET",
+      url: `https://instagram243.p.rapidapi.com/userposts/${id}/20/%7Bend_cursor%7D`,
+      headers: {
+        "x-rapidapi-key": "b1c26628e0msh3fbbf13ea24b4abp184561jsna2ebae86e910",
+        "x-rapidapi-host": "instagram243.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const { data } = await axios.request(options);
+
+      res.status(200).send({
+        success: true,
+        message: "Fetched User Posts Successfully",
+        posts: data?.data,
+      });
+    } catch (error) {
+      res.status(500).send({
+        success: false,
+        message: "Something went wrong",
+        error,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
+});
+
+app.use("/post-details/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const options = {
+      method: "GET",
+      url: `https://instagram243.p.rapidapi.com/postdetail/${id}`,
+      headers: {
+        "x-rapidapi-key": "b1c26628e0msh3fbbf13ea24b4abp184561jsna2ebae86e910",
+        "x-rapidapi-host": "instagram243.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const { data } = await axios.request(options);
+      res.status(200).send({
+        success: true,
+        message: "Post Details Found",
+        data,
+      });
+    } catch (error) {
+      res.status(500).send({
+        success: false,
+        message: "Something went wrong",
+        error,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
   }
 });
 
